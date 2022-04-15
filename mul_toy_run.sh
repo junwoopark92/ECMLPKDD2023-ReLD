8159c32dfdabc26e7eb41041b6e9a91aa1f3bd9f
junwoopark <pjw921116@gmail.com>
2022-04-15 15:32:39 +0900

    t test gap

--- mul_toy_run.sh
+++ mul_toy_run.sh
@@ -1,6 +1,6 @@
-sh mul_toy.sh 2 Autoformer $1 base > $1_autoformer.log &
+sh mul_toy.sh 0 Autoformer $1 base > $1_autoformer.log &
 sh mul_toy.sh 2 Informer $1 base > $1_informer.log &
-sh mul_toy.sh 0 Transformer $1 base > $1_transformer.log &
-sh mul_toy.sh 3 Autoformer $1 lds > $1_autoformer_lds.log &
-sh mul_toy.sh 3 Informer $1 lds > $1_informer_lds.log &
-sh mul_toy.sh 0 Transformer $1 lds > $1_transformer_lds.log &
+sh mul_toy.sh 3 Transformer $1 base > $1_transformer.log &
+sh mul_toy.sh 0 Autoformer $1 lds > $1_autoformer_lds.log &
+sh mul_toy.sh 2 Informer $1 lds > $1_informer_lds.log &
+sh mul_toy.sh 3 Transformer $1 lds > $1_transformer_lds.log &
