319fdd3395d0166292de809f7e39233a2073abc2
junwoopark <pjw921116@gmail.com>
2022-04-15 15:46:38 +0900

    sh

--- uni_run.sh
+++ uni_run.sh
@@ -1,7 +1,7 @@
 sh uni.sh 0 $1 Transformer lds > $1_transformer_lds.log &
-sh uni.sh 1 $1 Informer lds > $1_Informer_lds.log &
-sh uni.sh 5 $1 Autoformer lds > $1_autoformer_lds.log &
+sh uni.sh 2 $1 Informer lds > $1_Informer_lds.log &
+sh uni.sh 3 $1 Autoformer lds > $1_autoformer_lds.log &
 sh uni.sh 0 $1 Transformer base > $1_transformer.log &
-sh uni.sh 5 $1 Informer base > $1_Informer.log &
-sh uni.sh 1 $1 Autoformer base > $1_autoformer.log &
+sh uni.sh 3 $1 Informer base > $1_Informer.log &
+sh uni.sh 2 $1 Autoformer base > $1_autoformer.log &
 
